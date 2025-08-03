e738ebacd72426aed661f7227caa33da06638e22
Vikashiu <163726384+Vikashiu@users.noreply.github.com>
2025-08-03 22:04:08 +0530

    google calender webhook removed

--- primaryBackend/src/routes/oauth2callbackRouter.ts
+++ primaryBackend/src/routes/oauth2callbackRouter.ts
@@ -32,27 +32,15 @@ app.get("/", async (req, res) => {
       create: {
         userId: userId as string,
         accessToken: tokens.access_token!,
-        refreshToken: tokens.refresh_token,
+        refreshToken: tokens.refresh_token!,
         expiryDate: tokens.expiry_date ? new Date(tokens.expiry_date) : null,
       },
     });
     console.log("✅ Credentials saved for user:", userId);
-
-    const calendar = google.calendar({ version: "v3", auth: oauth2Client });
-
-    // 🔔 Register webhook (events.watch)
-    const response = await calendar.events.watch({
-      calendarId: "primary",
-      requestBody: {
-        id: uuidv4(), // unique ID for this channel
-        type: "web_hook",
-        address: `https://c2c9-2409-40c4-10f9-e493-2964-a5e7-65a-fa39.ngrok-free.app/catch/${userId}/${zapId}`, // replace with your current ngrok URL
-      },
-    });
-
+    
     res.send("OAuth complete. Webhook registered. Check console.");
     } catch (error) {
-      console.error("❌ Error in OAuth callback:", error);
+      console.log("❌ Error in OAuth callback:", error);
       res.status(500).send("Authentication failed.");
     }
 });
