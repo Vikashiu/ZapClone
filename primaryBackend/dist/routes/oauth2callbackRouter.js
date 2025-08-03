5f522f674f6746d0e39e84054ae772e14308ceca
Vikashiu <163726384+Vikashiu@users.noreply.github.com>
2025-08-03 22:47:07 +0530

    update dist

--- primaryBackend/dist/routes/oauth2callbackRouter.js
+++ primaryBackend/dist/routes/oauth2callbackRouter.js
@@ -12,16 +12,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.oauth2callbackRouter = void 0;
 const express_1 = require("express");
 const client_1 = require("@prisma/client");
+const authMiddleware_1 = require("../authMiddleware");
 const app = (0, express_1.Router)();
 const prisma = new client_1.PrismaClient();
 const { google } = require("googleapis");
 const { v4: uuidv4 } = require("uuid");
 require("dotenv").config();
 const oauth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI);
-app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.get("/", authMiddleware_1.authMiddleware, (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { code } = req.query;
-    const userId = "1";
-    const zapId = "";
+    // @ts-ignore
+    const userId = req.id;
     // console.log(code);
     try {
         const { tokens } = yield oauth2Client.getToken(code);
